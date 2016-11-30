98c05475c21c8244fcd981bda9b6f7be934fc1e2
Derkjan <derkjanriesthuis@gmail.com>
2016-11-30 23:18:09 +0100

    add simulation function, and refactored simulation into a class

--- assignments/assignment2/simulate_MMc_queues.py
+++ assignments/assignment2/simulate_MMc_queues.py
@@ -12,7 +12,8 @@ def get_service_times(service_time_func, *args):
         yield service_time_func(*args)
 
 def get_exponential(scale):
-    return np.random.exponential(scale=scale)
+    """generates number from exponential distribution with scale is the rate (lambda)"""
+    return np.random.exponential(scale=1/scale)
 
 def get_deterministic(time):
     return time
@@ -32,46 +33,83 @@ def get_hyper_exponential(time_short, time_long, chance_first):
 
 wait_times = []
 
-def customer(env, count, counters, service_time, create_time, service_mode):
-    if service_mode=='shortest_first':
-        priority = service_time
-    else:    
-        priority = count
+class SimulateQueue():
+    def __init__(self, burn_in_time):
+        self.wait_times = []            
+        self.burn_in_time = burn_in_time
 
-    yield env.timeout(create_time)    
-    start = env.now
-    with counters.request(priority=priority) as req:
-        yield req
-        end = env.now
-        if env.now > burn_in_time:
-            wait_times.append(end-start)
-        print("customer {} being served at {} wait time was {}".format(
-            count, env.now, end-start)
-        )
-        yield env.timeout(service_time)
+    def customer(self, env, count, counters, service_time, create_time, service_mode):
+        if service_mode=='shortest_first':
+            priority = service_time
+        else:    
+            priority = count
 
-def set_waiting_times(burn_in_time, number_of_customers, capacity, arrival_time_gen, 
-                      service_time_gen):
-    """get waiting times of customers"""    
-    
-    env = simpy.Environment()
-    counters = simpy.PriorityResource(env, capacity=capacity)
-    for i in range(number_of_customers):
-        env.process(customer(
-            env, i, counters, next(service_time_gen), next(arrival_time_gen), 
-            'shortest_first'
+        yield env.timeout(create_time)    
+        start = env.now
+        with counters.request(priority=priority) as req:
+            yield req
+            end = env.now
+            if env.now > self.burn_in_time:
+                self.wait_times.append(end-start)
+            #print("customer {} being served at {} wait time was {}".format(
+            #    count, env.now, end-start)
+            #)
+            yield env.timeout(service_time)
+
+    def set_waiting_times(self, number_of_customers, capacity, arrival_time_gen, 
+                          service_time_gen, service_mode):
+        """get waiting times of customers
+        
+        params:
+            capacity: amount of servers being  used
+            service_time_gen: generator object yielding service times
+            arrival_time_gen: generator object yielding arrival times
+            serice_mode: either FIFO or shortest_first
+        """    
+        
+        env = simpy.Environment()
+        counters = simpy.PriorityResource(env, capacity=capacity)
+        for i in range(number_of_customers):
+            env.process(self.customer(
+                env, i, counters, next(service_time_gen), next(arrival_time_gen), 
+                service_mode
+                )
             )
-        )
 
-    env.run()
+        env.run()
+
+def simulate_RBM(batches, replications, burn_in_time, args_to_set_waiting_times):
+    """ Use RBM sampling and return estimate of the mean and standard variance"""
+    mean_waiting_times_li = []    
+    batch_size = int(args_to_set_waiting_times[0]/batches)
+    for i in range(replications):
+        sim_queue = SimulateQueue(burn_in_time)
+        sim_queue.set_waiting_times(*args_to_set_waiting_times)
+        wait_times_list = [] 
+        for i in range(batches-1):
+            wait_times_list.append(
+                sim_queue.waiting_times[batch_size*i:batch_size*(i+1)]
+            )           
+        wait_times_list.append(
+            sim_queue.waiting_times[batch_size*(batches-1):]
+        ) 
+        mean_waiting_times_li.append(np.mean(wait_times_list, 0))
+
+    return np.mean(mean_waiting_times_li), np.std(mean_waiting_times_li) 
 
 if __name__=='__main__':
-    burn_in_time = 0
-    number_of_customers = 10
-    capacity = 2
-    set_waiting_times(burn_in_time, 10, 1, get_arrival_times(get_exponential, 1.6), 
-                      get_service_times(get_exponential, 1))
+    """
+    notes:service_time is assumed to have rate 1 and then load and capacity are 
+        used to calculate the arrival rate
+    """
 
-    print(wait_times)
+    sim_queue = SimulateQueue(0)
+    sim_queue.set_waiting_times(number_of_customers=10, capacity=2, 
+        arrival_time_gen=get_arrival_times(get_exponential, 1.95),
+        service_time_gen=get_service_times(get_exponential, 1), 
+        service_mode ='FIFO'
+    )
+
+    print(sim_queue.wait_times)
 
 
